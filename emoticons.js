2cb81fdf201b7f1f6bfd5b1a6121ed4fbd46bff3
itisacrouton <itisacrouton@proton.me>
2025-01-16 10:17:32 +1100

    started program to generate code for me from an index of gifs

--- emoticons.js
+++ emoticons.js
@@ -1,33 +1,50 @@
 var emoticons_flex = document.getElementById("emoticons");
 
 var tag_form_element = document.getElementById("tag_form");
-var tag_alcohol_element = document.getElementById("tag_alcohol");
 
 tag_form_element.addEventListener("change", function() {form_change()});
 
-for (let i=0; i<9; i++){
-    let elem = document.createElement("img");
-    elem.src = 'gifs/'+i+'.gif';
-    elem.classList.add("emoticon-image");
-    if (elem.width > elem.height){
-        elem.height = 100;
-    }else{
-        elem.width = 100;
-    }
-    emoticons_flex.appendChild(elem);
+var emoticons_index = {
+    'all': new Set(['0.gif']),
+    'desperate': new Set(['0.gif']),
+    'alcohol': new Set(['0.gif'])
 }
 
 function form_change(){
-    var selected_tags = [];
-    for (var el of tag_form_element.elements){
-        if (el.id == "clear_selection"){
-            continue;
+    setTimeout(function(){
+        // gross timeout but easy way to make reset run after selections cleared
+        var selected_tags = [];
+        for (var el of tag_form_element.elements){
+            if (el.id == "clear_selection"){
+                continue;
+            }
+
+            if (el.checked){
+                selected_tags.push(el.labels[0].innerHTML)
+            }
+            //console.log(el);
         }
-        if (el.checked){
-            selected_tags.push(el.labels[0].innerHTML)
+        //console.log(selected_tags);
+        while (emoticons_flex.firstChild){
+            emoticons_flex.firstChild.remove();
         }
-        console.log(el);
-    }
-    console.log(selected_tags);
+
+        var relevant_emoticons = emoticons_index['all'];
+
+        for (tag of selected_tags){
+            relevant_emoticons = relevant_emoticons.intersection(emoticons_index[tag]);
+        }
+        
+
+        for (path of relevant_emoticons){
+            //console.log(path);
+            let elem = document.createElement("img");
+            elem.src = 'gifs/'+path;
+            elem.classList.add("emoticon-image");
+            emoticons_flex.appendChild(elem);
+        }
+    }, 0);
     /*console.log(tag_form_element.elements['tag_alcohol'].checked);*/
 }
+
+form_change();
---
+++ gif_indexer.py
@@ -0,0 +1,28 @@
+import yaml
+with open('gif_index.yaml') as f:
+    gif_index = yaml.safe_load(f)
+
+js_index = {'all':[]}
+for gif, themes in gif_index.items():
+    js_index['all'].append(gif)
+    for theme in themes:
+        try:
+            js_index[theme].append(gif)
+        except KeyError:
+            js_index[theme] = [gif]
+
+out_str = 'var emoticons_index = {\n'
+
+for theme, gifs in js_index.items():
+    out_str += f"    '{theme}': new Set({gifs}),\n"
+
+out_str = out_str[:-2]+'\n}'
+
+print(out_str)
+
+html_str = ''
+for theme in js_index.keys():
+    html_str += f'<input type="checkbox" id="tag_{theme}">\n'
+    html_str += f'<label for="tag_{theme}">{theme}</label>\n'
+
+print(html_str)
